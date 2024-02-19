3b25e8a3d525193dbb525dd61ab00536ae7fb51f
Toussaintduk <146627611+Toussaintduk@users.noreply.github.com>
2024-02-19 21:04:24 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+setup(
+name='mypackage',
+version='0.1',
+packages=find_packages(exclude=['tests*']),
+license='MIT',
+description='EDSA example python package',
+long_description=open('readme.MD').read(),
+install_requires=['numpy'],
+url='https://example.com',
+author='Dukundimna Toussaint',
+author_email='dukundesaint@gmail.com',
+
+
+
+)
